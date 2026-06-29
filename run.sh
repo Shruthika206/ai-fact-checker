513620962f9d1ea9fd9f3fa47e4d664de1f1860c
Shruthika <jatoth.shruthika@accenture.com>
2026-06-29 00:02:21 -0700

    Update run.sh for Azure App Service startup

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install dependencies
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt
+
+# Run Streamlit app
 streamlit run AgentWeb.py \
-  --server.port=$PORT \
-  --server.address=0.0.0.0
+  --server.port=8000 \
+  --server.address=0.0.0.0 \
+  --logger.level=info
